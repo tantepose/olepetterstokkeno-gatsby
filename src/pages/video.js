49203c404b34b63d3719cff83862fe89c9318b65
Ole Petter Baugerød Stokke <doktoresel@gmail.com>
2021-03-15 15:54:28 +0100

    now video gets content, too

--- src/pages/video.js
+++ src/pages/video.js
@@ -5,6 +5,8 @@ import SEO from "../components/seo"
 import TitleBox from "../components/titlebox"
 import VideoImage from "../images/video.gif"
 
+import content from "../content/content.json"
+
 const video = () => (
   <Layout currentPage="video">
     <SEO title="Ole Petter Baugerød Stokke filmer" />
@@ -15,21 +17,19 @@ const video = () => (
         text="video"
     />
 
-    <p>Her er et par ting jeg har filma.</p>
-    <p>Her er et par ting jeg har filma.</p>
-    <p>Her er et par ting jeg har filma.</p>
-    <p>Her er et par ting jeg har filma.</p>
-    <p>Her er et par ting jeg har filma.</p>
-    <p>Her er et par ting jeg har filma.</p>
-    <p>Her er et par ting jeg har filma.</p>
-    <p>Her er et par ting jeg har filma.</p>
-    <p>Her er et par ting jeg har filma.</p>
-    <p>Her er et par ting jeg har filma.</p>
-    <p>Her er et par ting jeg har filma.</p>
-    <p>Her er et par ting jeg har filma.</p>
-    <p>Her er et par ting jeg har filma.</p>
-    <p>Her er et par ting jeg har filma.</p>
-    <p>Her er et par ting jeg har filma.</p>
+    {Object.keys(content.video).map((category, index) => { //map thru content.tekst keys (categories) as array
+      return (
+        <>
+          <h1>{category}</h1>
+          <ul>
+            {content.video[category].map((item, index) => { // map thru items in each category
+              return <li><a href={item.url}>{item.title}</a></li>
+            })}
+          </ul>
+        </>
+      )
+    })}
+
   </Layout>
 )
 
